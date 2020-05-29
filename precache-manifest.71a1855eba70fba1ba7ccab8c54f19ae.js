5120b357d7439bc702a38483826c0ef2c7dc05fd
Jasmine Terry <jasmine-terry91@gmail.com>
2020-05-29 01:22:16 -0400

    Updates

---
+++ precache-manifest.71a1855eba70fba1ba7ccab8c54f19ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d63f87d81070dac622a148fb58ce24c",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "311d54bd66f2618c48d9",
+    "url": "/game-of-life/static/css/main.323bcf7d.chunk.css"
+  },
+  {
+    "revision": "9c24a0bdf27b281cffff",
+    "url": "/game-of-life/static/js/2.7e1c3a10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game-of-life/static/js/2.7e1c3a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "311d54bd66f2618c48d9",
+    "url": "/game-of-life/static/js/main.c6158d70.chunk.js"
+  },
+  {
+    "revision": "74c0ec62b28c760df103",
+    "url": "/game-of-life/static/js/runtime-main.ccc8ab68.js"
+  }
+]);
